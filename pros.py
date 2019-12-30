9b86aacb69dc8feb660def4df9358b6c21db01e2
Akash G Krishnan <krishnanag1996@gmail.com>
2019-12-30 23:34:25 +0530

    todays coding challenges

---
+++ pros.py
@@ -0,0 +1,19 @@
+
+
+    
+
+
+pros = ["Jack",  "Leon",  "Maria"]
+preference = [["Computer repair","Handyman","House cleaning"], 
+ ["Computer lessons","Computer repair","Data recovery service"], 
+ ["Computer lessons","House cleaning"]]
+for i,v in enumerate(preference):
+     print(f"i: {i} v: {v}")
+
+'''
+ [[["Computer lessons"],["Leon","Maria"]], 
+ [["Computer repair"],["Jack","Leon"]], 
+ [["Data recovery service"],["Leon"]], 
+ [["Handyman"],["Jack"]], 
+ [["House cleaning"],["Jack","Maria"]]]
+ '''
