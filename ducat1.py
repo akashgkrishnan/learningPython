afffca21608ff5511d45f43a8e6bfd08c4f35932
Akash G Krishnan <krishnanag1996@gmail.com>
2020-01-04 16:00:56 +0530

    todays learning at ducat

---
+++ ducat1.py
@@ -0,0 +1,26 @@
+count = 0
+while count <5:
+    count += 1
+    print(count)
+    break
+else:
+    print("this is the else loop")
+
+
+for i in "Akash":
+    print(i)
+    
+
+for i in 10,20,20,34.6:
+    print(i)
+
+for i in "Akash","g","krishnan":
+    print(i)
+
+for i in range(5,8):
+    print(i)
+
+for i in range(5,2,-1):
+    print(i)
+for i in range(-6, 0 , -1):
+    print(i)
