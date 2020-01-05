ab3661fbabc8e7ec413e546a1ef0ecabe00cd031
Akash G Krishnan <krishnanag1996@gmail.com>
2020-01-05 16:36:37 +0530

    new

---
+++ pattern3.py
@@ -0,0 +1,5 @@
+for i in range(65,72):
+    for j in range(65,i):
+        print(f"{chr(j)} ", end = '')
+    print()
+
---
+++ pattern4.py
@@ -0,0 +1,10 @@
+for i in range(6,1, -1):
+    for k in range(5):
+        print(" ",end = ' ')
+
+
+   
+    
+     
+    
+
