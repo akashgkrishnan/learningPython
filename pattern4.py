ba1a021a3a08c1aa5d6c15737fb8ec6f0e1e1037
Akash G Krishnan <krishnanag1996@gmail.com>
2020-01-05 20:41:56 +0530

    new

--- pattern4.py
+++ pattern4.py
@@ -1,6 +1,10 @@
-for i in range(6,1, -1):
-    for k in range(5):
+for i in range(5):
+    for k in range(i):
         print(" ",end = ' ')
+        for j in range(i):
+            print(j)
+        print()
+    print()
 
 
    
