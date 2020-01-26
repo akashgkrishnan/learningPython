3faa6ab44f608842bbb616b8f3f28346bb09f5cf
Akash G Krishnan <krishnanag1996@gmail.com>
2020-01-26 23:27:26 +0530

    udemys and more

---
+++ listcomp.py
@@ -0,0 +1,6 @@
+l1 = [1,2,3,4]
+l2 = [3,4,5,6]
+print((set(l1).intersection(set(l2))))
+
+
+print([val[::-1].lower() for val in ["Elie", "Tim", " Matt"]])
---
+++ listex.py
@@ -0,0 +1,7 @@
+instructors = ["Colt", "Blue", "Lisa"]
+
+instructors.pop()
+
+instructors.pop(0)
+
+instructors.insert(0, "Done")
