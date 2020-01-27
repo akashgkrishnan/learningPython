5495fbe99cee689d266cf21b063d315dd8594604
Akash G Krishnan <krishnanag1996@gmail.com>
2020-01-27 23:15:09 +0530

    udemy ex

---
+++ dictex.py
@@ -0,0 +1,12 @@
+# DON'T TOUCH PLEASE!
+donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
+# DON'T TOUCH PLEASE!
+sum = 0
+for donation in donations.values():
+    sum += donation
+
+print(sum)
+
+
+
+# Use a loop to add together all the donations and store the resulting number in a variable called total_donations
---
+++ dictex5.py
@@ -0,0 +1,5 @@
+a = ['ak', 'ch']
+b = ["Akash", "Christy"]
+
+c = {a[i] : b[i] for i in range(2)}
+print(c)
