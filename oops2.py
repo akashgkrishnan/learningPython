c650f11a727c9e1c5b4fb8ff26c169dfe02da01b
Akash G Krishnan <krishnanag1996@gmail.com>
2020-03-31 01:52:04 +0530

    rest

---
+++ oops2.py
@@ -0,0 +1,57 @@
+'''
+object oriented programing
+
+it is a programing style that simplify development and maintainence
+of real world application
+
+it follows some rules:
+
+> object
+> class 
+> ineruitance
+> encapsulation
+> abstraction
+> polymorphism
+
+>  association
+
+
+
+python mostly focuses on first 3 rules
+
+
+object 
+
+is an instance of a class
+any real world entity having attribute (data and method)
+dynamically alocated block in memory
+
+
+class
+ it is a blueprint or template to create objects
+ logical concept that defines attribute of similar type
+
+
+
+
+ class classmate:
+ 	attributes
+
+ 	or 
+
+ class classname():
+ 	attribute
+
+
+
+obj = classnam()
+
+object is just a reference to newly created object
+
+
+attribute is either a datamember or member functions
+
+
+
+
+'''
